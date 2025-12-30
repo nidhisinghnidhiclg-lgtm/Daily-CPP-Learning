0f379e77c0baa889d28fbb06bc3f953c3ec6026f
nidhisinghnidhiclg-lgtm <nidhisinghnidhiclg@gmail.com>
2025-12-30 14:08:18 +0530

    Add files via upload

---
+++ arithmeticoprerators.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x=6,y=7;
+    cout<<x+y<<endl;
+    cout<<x-y<<endl;
+    cout<<x*y<<endl;
+    cout<<x/y<<endl;
+
+
+
+
+
+
+}
+    
+    
+
+    
